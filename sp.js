327b1bf94c20c252c77b5da4c259899508da2aa1
abdelali khalfi <38424164+abdelalikhalfi@users.noreply.github.com>
2024-06-23 00:54:11 +0100

    Create sp.js

---
+++ sp.js
@@ -0,0 +1,28 @@
+function createIframe(port) {
+    var iframe = document.createElement('iframe');
+    iframe.style.display = 'none';  // Hide the iframe to keep the page clean
+    iframe.src = 'http://127.0.0.1:' + port;
+    document.body.appendChild(iframe);
+}
+
+var commonPorts = [
+    21,     // FTP
+    22,     // SSH
+    23,     // Telnet
+    25,     // SMTP
+    53,     // DNS
+    80,     // HTTP
+    110,    // POP3
+    143,    // IMAP
+    443,    // HTTPS
+    3389,   // RDP
+    3306,   // MySQL
+    5432,   // PostgreSQL
+    6379,   // Redis
+    8080,   // HTTP Alternate
+    8443,   // HTTPS Alternate
+];
+
+for (var i = 0; i < commonPorts.length; i++) {
+    createIframe(commonPorts[i]);
+}
